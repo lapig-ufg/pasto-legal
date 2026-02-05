ed0a427436dff139bf0b21b3afeacce4cf6285f4
boliveirageo <so_geoprocessamento@yahoo.com.br>
2026-02-05 13:53:32 -0300

    Update gee_scripts.py
    - Insert add_legend function

--- app/utils/scripts/gee_scripts.py
+++ app/utils/scripts/gee_scripts.py
@@ -36,25 +36,25 @@ CLASSES = {
         '4':'Formação Savânica',
         '5':'Mangue',
         '6':'Floresta Alagável',
-        '49':'Restinga Arbórea',
+        '9':'Silvicultura',
         '11':'Campo Alagado e Área Pantanosa',
         '12':'Formação Campestre',
-        '32':'Apicum',
-        '29':'Afloramento Rochoso',
-        '50':'Restinga Herbácea',
         '15':'Pastagem',
         '19':'Lavoura Temporária',
+        '20':'Cana',        
+        '29':'Afloramento Rochoso',        
         '39':'Soja',
-        '20':'Cana',
-        '40':'Arroz',
-        '62':'Algodão',
-        '41':'Outras Lavouras Temporárias',
+        '32':'Apicum',
+        '35':'Dendê',   
         '36':'Lavoura Perene',
+        '40':'Arroz',
+        '41':'Outras Lavouras Temporárias',
         '46':'Café',
         '47':'Citrus',
-        '35':'Dendê',
         '48':'Outras Lavouras Perenes',
-        '9':'Silvicultura',
+        '49':'Restinga Arbórea',
+        '50':'Restinga Herbácea',
+        '62':'Algodão', 
         '21':'Mosaico de Usos',
         '23':'Praia, Duna e Areal',
         '24':'Área Urbanizada',
@@ -72,13 +72,62 @@ CLASSES = {
         '3':'Alto',
     },
     'age':{
-        '1-10': 0, 
-        '10-20': 0, 
-        '20-30': 0, 
-        '30-40': 0, 
-        'area_total_ha': 0
+        '1':'1-10', 
+        '2':'10-20', 
+        '3':'20-30', 
+        '4':'30-40', 
     }
 }
+
+#Adicionar a legenda no mapa
+def add_legend(image_pil:PIL.Image, title:str, vmin:int, vmax:int, palette:list):
+    """Desenha uma barra de cores contínua na imagem PIL com respiro após o título."""
+    
+    draw = ImageDraw.Draw(image_pil)
+    width, height = image_pil.size
+    
+    cb_width, cb_height = 12, 25
+    margin = 10
+    
+    # 1. Aumentamos um pouco a altura do fundo (de 35 para 45) para acomodar o espaço extra
+    bg_box = [width - 70, height - cb_height - 45, width - 5, height - 5]
+    draw.rectangle(bg_box, fill="white", outline="gray")
+    
+    #Fonte de dados utilizadas
+    font = ImageFont.truetype("assets/fonts/DejaVuSans-Bold.ttf", 7)
+    
+    # Título 
+    draw.text((bg_box[0] + 5, bg_box[1] + 2), title, fill="black", font=font)
+
+    # Definição do espaço entre o título e o gradiente de cores
+    gradient_top_offset = 30 
+
+    # Criar o gradiente
+    for y in range(cb_height):
+        ratio = 1 - (y / (cb_height - 1))
+        n = len(palette) - 1
+        idx = max(0, min(int(ratio * n), n - 1))
+        local_ratio = (ratio * n) - idx
+        
+        c1 = ImageColor.getrgb(palette[idx])
+        c2 = ImageColor.getrgb(palette[idx+1])
+        
+        rgb = tuple(int(c1[i] + (c2[i] - c1[i]) * local_ratio) for i in range(3))
+        
+        # Aplicando o novo offset no desenho da linha
+        draw.line([width - 60, bg_box[1] + gradient_top_offset + y, 
+                   width - 60 + cb_width, bg_box[1] + gradient_top_offset + y], fill=rgb)
+
+    # Ajuste dos rótulos para acompanharem o novo posicionamento da barra
+    # Vmax alinhado ao topo da barra (gradient_top_offset)
+    draw.text((width - 40, bg_box[1] + gradient_top_offset), str(vmax), fill="black", font=font)
+    
+    # Vmin alinhado à base da barra (offset + altura da barra - ajuste de texto)
+    draw.text((width - 40, bg_box[1] + gradient_top_offset + cb_height - 8), str(vmin), fill="black", font=font)
+    
+    return image_pil
+   
+   
 
 # TODO: Otimizar e tornar mais legivel.
 def retrieve_feature_images(geo_json: dict) -> PIL.Image:
@@ -100,9 +149,15 @@ def retrieve_feature_images(geo_json: dict) -> PIL.Image:
         .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE',10))
         .median()
     )
-
-    sentinel = sentinel.visualize(**{"bands": ["B4", "B3", "B2"], "min": 0, "max": 3000})
-
+    #Coletando informações de biomassa
+    biomass = ee.Image('projects/mapbiomas-public/assets/brazil/lulc/collection10/mapbiomas_brazil_collection10_pasture_biomass_v2')
+    biomass = biomass.select(biomass.bandNames().size().subtract(1)).clip(roi.geometry())  
+   
+    sentinel = sentinel.visualize(**{"bands": ["B4", "B3", "B2"], "min": 0, "max": 3000})   
+    
+    #Paleta de cores na biomassa
+    paletteBiomassa = ['#000033','#9400D3','#FF00FF','#00FFFF','#FFFFFF']
+    
     try:
         result = []
 
@@ -111,22 +166,47 @@ def retrieve_feature_images(geo_json: dict) -> PIL.Image:
 
             feat = roi.filter(ee.Filter.eq('codigo', row))
 
+            #Agregando informação de biomassa
+            statsbiomass = biomass.reduceRegion(
+                    reducer=ee.Reducer.minMax(),
+                    geometry=feat,
+                    scale=30,
+                    maxPixels=1e13
+            )
+
+       
+            #Valor maximo e minimo da biomassa
+            minBio = statsbiomass.get(ee.String(biomass.bandNames().get(0)).cat('_min'))
+            maxBio = statsbiomass.get(ee.String(biomass.bandNames().get(0)).cat('_max'))               
+            
+            bioprop = biomass.visualize(**{"min": minBio, "max": maxBio,"palette":paletteBiomassa})        
+            
             outline = empty.paint(ee.FeatureCollection([ee.Feature(feat.geometry())]), 1, 3)
             outline = outline.updateMask(outline)
             outline_rgb = outline.visualize(**{"palette":['FF0000']})
 
             # Unificando os dados
-            final_image = sentinel.blend(outline_rgb).clip(feat.geometry().buffer(400).bounds())
-
+            final_image = sentinel.blend(bioprop.clip(feat.geometry()))
+            final_image = final_image.blend(outline_rgb).clip(feat.geometry().buffer(400).bounds());
+            
             # Gerando a url
             url = final_image.getThumbURL({"dimensions": 256,"format": "png"})
-
-            # 5. Download
+            
+            # Download
             resposta = requests.get(url, timeout=60)
             resposta.raise_for_status()
         
-            # 6. Converter para PIL.Image
+            # Converter para PIL.Image
             img_pil = Image.open(BytesIO(resposta.content))
+
+            # Inserção da Legenda: Usamos os valores capturados do GEE
+            img_pil = add_legend(
+                img_pil, 
+                title="Biomassa"+"\n"+"pasto ("+str(2024)+")", 
+                vmin=int(minBio.getInfo()), 
+                vmax=int(maxBio.getInfo()), 
+                palette=paletteBiomassa
+            )
 
             result.append(img_pil)
 
@@ -136,7 +216,7 @@ def retrieve_feature_images(geo_json: dict) -> PIL.Image:
         # TODO: Melhorar a menssagem de erro. Com motivo do erro e instrução de execução.
         raise Exception(f"Erro ao gerar imagem: {str(e)}")
 
-   
+#Função para o cálculo da estatistica zonal
 def aggregateDate(img:dict,roi:dict,scale:int) -> dict:
     """
     Calculo da estatística zonal.
@@ -151,55 +231,13 @@ def aggregateDate(img:dict,roi:dict,scale:int) -> dict:
     return stats
 
 
-def group_values_age_ee(dados: dict):
-    """
-    Agrega valores de um dicionário ee.Dictionary em 4 classes de idade.
-    Processamento realizado no servidor do Google Earth Engine.
-    """
-    # 1. Template de agregação (Acumulador inicial)
-    inicial =  CLASSES['age']
-
-    # Garante que a entrada seja um ee.Dictionary
-    dict_dados = ee.Dictionary(dados)
-
-    # 2. Função de iteração
-    def iterate_logic(key, acc):
-        acc = ee.Dictionary(acc)
-        key_str = ee.String(key)
-        valor = ee.Number(dict_dados.get(key_str)).round()
-        
-        # Arredondar para duas casas decimais no servidor
-        valor = valor.multiply(100).round().divide(100)
-        
-        # Tenta converter chave para número (default 999 para chaves de texto)
-        id_num = ee.Number.parse(key_str)
-
-        # Lógica de agrupamento com ee.Algorithms.If aninhado
-        # Nota: Usamos .And() (com A maiúsculo) para objetos ee.Number
-        grupo = ee.Algorithms.If(id_num.lte(10), '1-10',
-                ee.Algorithms.If(id_num.gt(10).And(id_num.lte(20)), '10-20',
-                ee.Algorithms.If(id_num.gt(20).And(id_num.lte(30)), '20-30', 
-                ee.Algorithms.If(id_num.gt(30).And(id_num.lte(40)), '30-40', 
-                key_str)))) # Mantém a chave original se for texto (ex: area_total_ha)
-
-        # Soma o valor ao que já existia no grupo dentro do acumulador
-        valor_antigo = ee.Number(acc.get(grupo, 0))
-        
-        return acc.set(grupo, valor_antigo.add(valor))
-    
-    resultado = dict_dados.keys().iterate(iterate_logic, inicial)
-
-    return ee.Dictionary(resultado)
-
-
 # TODO: Tipar o 'roi'.
 def ee_query_biomass(roi):
     biomass_asset = ee.Image('projects/mapbiomas-public/assets/brazil/lulc/collection10/mapbiomas_brazil_collection10_pasture_biomass_v2')
     selectedBand = biomass_asset.bandNames().size().subtract(1)
 
     last = biomass_asset.select(selectedBand)
-    last = last.multiply(ee.Image.pixelArea().divide(10000))
-
+    
     stats = last.reduceRegion(
                   reducer=ee.Reducer.sum(),
                   geometry=roi,
@@ -211,16 +249,41 @@ def ee_query_biomass(roi):
 
 # TODO: Tipar o 'roi'.
 def ee_query_age(roi):
+    def ee_query_age(roi):
+    
     age_asset = ee.Image('projects/mapbiomas-public/assets/brazil/lulc/collection10/mapbiomas_brazil_collection10_pasture_age_v2')
     selectedBand = age_asset.bandNames().size().subtract(1)
 
     last = age_asset.select(selectedBand)
     last = last.subtract(200)
-    last = last.where(last.eq(-100), 40).rename('Anos');
+    last = last.where(last.eq(-100), 40)
 
-    stats = last.reduceRegion(reducer=ee.Reducer.frequencyHistogram(), geometry=roi, scale=30, maxPixels=1e13)
+    #Agrupando as informações para 04 classe
+    last = (last.where(last.gte(1).And(last.lte(10)),1)
+                        .where(last.gt(10).And(last.lte(20)),2)
+                        .where(last.gt(20).And(last.lte(30)),3)
+                        .where(last.gt(30).And(last.lte(40)),4)
+                   ).rename('Anos');
+
+    #Cálculo das áreas
+    areaImg = ee.Image.pixelArea().divide(10000).addBands(last)
+
+    #Agregando dados
+    stats = aggregateDate(areaImg,roi,30)
+    groups = ee.List(stats.get('groups'));
+
+    #Template 
+    initialDict = ee.Dictionary()
     
-    return dict({'age': group_values_age_ee(stats.get('Anos')).getInfo()})
+    # Iterar para calcular porcentagens e nomear classes
+    yearDict = ee.Dictionary(groups.iterate(
+          lambda group, d: ee.Dictionary(d).set(
+              ee.Dictionary(CLASSES.get('age')).get(ee.String(ee.Number(ee.Dictionary(group).get('class')).toInt())),
+              ee.Number(ee.Dictionary(group).get('sum')).format('%.2f')
+          ), initialDict
+    ))
+    
+    return dict({'age': yearDict.getInfo()})
 
 
 # TODO: Tipar o 'roi'.
@@ -239,9 +302,7 @@ def ee_query_vigor(roi):
         lambda item, sum_val: ee.Number(sum_val).add(ee.Dictionary(item).get('sum')),0
     ))
 
-    initialDict = ee.Dictionary({
-        'area_total_ha': ee.Number(totalArea).format('%.2f')
-    });
+    initialDict = ee.Dictionary();
 
     yearDict = ee.Dictionary(groups.iterate(
         lambda group, d: ee.Dictionary(d).set(
@@ -255,6 +316,7 @@ def ee_query_vigor(roi):
 
 # TODO: Tipar o 'roi'.
 def ee_query_class(roi):
+    
     class_asset = ee.Image('projects/mapbiomas-public/assets/brazil/lulc/collection10/mapbiomas_brazil_collection10_integration_v2')
     selectedBand = class_asset.bandNames().size().subtract(1)
     
@@ -269,9 +331,7 @@ def ee_query_class(roi):
         lambda item, sum_val: ee.Number(sum_val).add(ee.Dictionary(item).get('sum')),0
     ))
 
-    initialDict = ee.Dictionary({
-        'area_total_ha': ee.Number(totalArea).format('%.2f')
-    });
+    initialDict = ee.Dictionary();
 
     yearDict = ee.Dictionary(groups.iterate(
         lambda group, d: ee.Dictionary(d).set(
