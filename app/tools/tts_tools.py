c715ed41834cf3878109243fe0bf0728884e2f62
Tiago Gonçalves <tiagogonsalves2001@gmail.com>
2026-07-23 09:48:39 -0300

    fix: Empty audio with bytes

--- app/tools/tts_tools.py
+++ app/tools/tts_tools.py
@@ -5,7 +5,7 @@ from agno.tools import tool
 from agno.tools.function import ToolResult
 from agno.media import Audio
 
-@tool
+@tool(stop_after_tool_call=True)
 def generate_speech(text: str) -> ToolResult:
     """
     Gera áudio falado (conversão de texto em fala) a partir de um texto fornecido.
@@ -20,10 +20,4 @@ def generate_speech(text: str) -> ToolResult:
     Returns:
         ToolResult: Objeto de resultado contendo a fala gerada.
     """
-    buffer = io.BytesIO()
-    with wave.open(buffer, "wb") as wav_file:
-        wav_file.setnchannels(1)
-        wav_file.setsampwidth(2)
-        wav_file.setframerate(44100)
-
-    return ToolResult(content="", audios=[Audio(content=buffer.getvalue(), transcript=text)])
+    return ToolResult(content="", audios=[Audio(content=b"", transcript=text)])
